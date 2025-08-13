56d82266a48c01a2c3f9bc8410490d30dd14300f
sougata das <sougata.das@webskitters.in>
2025-08-13 13:50:51 +0530

    change counter declearation

--- src/components/hooks/Counter.jsx
+++ src/components/hooks/Counter.jsx
@@ -6,10 +6,10 @@ export default function Counter() {
   return (
     <>
       <h1>Increment Decrement Value {counter} </h1>
-      <button type="button" onClick={() => setCounter(counter + 1)}>
+      <button type="button" onClick={() => setCounter((oldCounter) => oldCounter + 1)}>
         Increment Decrement Button
       </button>
-      <button type="button" onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}>
+      <button type="button" onClick={() => setCounter((oldCounter) => (oldCounter > 0 ? oldCounter - 1 : 0))}>
         Decrement Button
       </button>
     </>
